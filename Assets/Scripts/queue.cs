0486ca1e5f4330904ccdc3f4660b55aa48382fb8
Arreth45 <arreth45@gmail.com>
2016-02-25 13:14:06 -0500

    updated stuff

--- Assets/Scripts/queue.cs
+++ Assets/Scripts/queue.cs
@@ -1,47 +1,44 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class queue : MonoBehaviour
 {
-    GameObject[] line;
-    float timer;
+    public List<GameObject> line = new List<GameObject>();
     int counter;
     int openSpace;
 
     // Use this for initialization
     void Start()
     {
-        line = GameObject.FindGameObjectsWithTag("person"); // change tag later or not
+        for (int i = 0; i > 10; i++)
+        {
+            line.Add(GameObject.FindGameObjectWithTag("person"));
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (line.Length < 10)
+        if (line.Count < 10)
         {
-            for (int i = 0; i > (10 - line.Length); i++)
+            for (int i = 0; i > (10 - line.Count); i++)
             {
-                //code to add more people
-                //line.add(kevinscript.poolparty[1]);
+                line.Add(GameObject.FindGameObjectWithTag("person"));
             }
-        }
-        foreach (GameObject person in line)
-        {
-
-            if(timer > person.patience()){
-                line.RemoveAt(person.place());
-                openSpace = counter;
-                MoveUp();
-            }
-            counter++;
         }
     }
 
     private void MoveUp()
     {
-        for (int i = 0; i > (line.Length + 1 - counter); i++)
+        for (int i = 0; i > (line.Count - counter); i++)
         {
             line[openSpace] = line[openSpace + 1];
             openSpace++;
         }
+    }
+
+    public void LeaveLine(GameObject person)
+    {
+        line.Remove(person);
     }
 }
